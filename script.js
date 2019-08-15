80db9e0f507100af546eb08f6116fc69c7c77205
jamieYeung <jamieyyw@gmail.com>
2019-08-14 21:00:32 -0400

    more bug testing, made buttons not work before sequence/start

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ app.init = () => {
   app.startGame = document.querySelector("#start");
   app.levelCount = document.querySelector(".level");
   app.feedback = document.querySelector(".feedback");
+  app.formInfo = document.querySelector(".formInfo")
   // different colors
   app.redButton = document.querySelector("#red");
   app.blueButton = document.querySelector("#blue");
@@ -63,9 +64,11 @@ app.init = () => {
   btn.className = "btn"
   // btn.innerHTML = "Submit"
   btn.onclick = function saveScoreOrNot() {
+
   const name = document.getElementById("name").value;
-    if (name == "") {
-      alert('Please enter a name to submit');
+  // form requires text input
+    if (name.trim() === "" || name === null) {
+      app.feedback.innerHTML = "<p>Please enter a name to submit</p>";
       return false;
     } else {
       app.newScore = {};
@@ -74,7 +77,9 @@ app.init = () => {
       app.newScore.name = document.getElementById("name").value;
       app.newScore.score = app.level;
       dbRef.push(app.newScore);
+      app.formInfo.innerHTML = '<h3>Score Saved!</h3>';
       console.log(app.newScore);
+
     }
   }
 
@@ -96,12 +101,19 @@ app.init = () => {
       // console.log('game is on')
       app.powerOnGame = true;
       app.gameOnOrOff.innerHTML = `<h3>Game is On</h3>`
+      document.getElementById('start').style.visibility = 'visible';
     } else if(app.powerOn.checked === false) {
       // console.log('game is off')
       app.powerOnGame = false;
-      app.gameOnOrOff.innerHTML = `<h3>Game is Off, turn the game on</h3>`
+      app.colorSequence = [];
+      app.levelCount.innerHTML = "";
+      app.formInfo.innerHTML = "";
+
+      app.gameOnOrOff.innerHTML = `<h3>Game is off, turn the game on</h3>`
       app.clearGame();
       clearInterval(app.colorInterval);
+      document.getElementById('start').style.visibility = 'visible';
+
     }
   }); 
 
@@ -113,33 +125,32 @@ app.init = () => {
     app.colorInterval = 0;
     app.level = 1;
     app.levelCount.innerHTML = `<h3>You are on : Level ${app.level}</h3>`;
+    app.feedback.innerHTML = "";
     app.correctPick = true;
+    app.formInfo.innerHTML ='';
+    
     // levels of the game
     for (let i = 0; i < 3; i++) {
       // 4 colors, so multiply by 4 and add 1 to get whole number between 1 - 5, math.floor rounds down the decimal, so number will be between 1 - 4(color options)
       app.colorSequence.push(Math.floor(Math.random() * 4) + 1);
     }
+    
     app.showSequence = true;
     console.log(app.colorSequence);
     app.colorInterval = setInterval(app.turn, 800);
   }
 
   app.startGame.addEventListener('click', (e) =>{
-
     if (app.powerOn.checked === false) {
       app.powerOnGame = false;
-      console.log('game is off turn it on')
     }
     else if(app.powerOnGame === true || app.winner === false){
-      console.log('game is on')
       app.start();
+      document.getElementById('start').style.visibility = 'hidden';
     }
   });
-
-
-
   app.turn = () => {
-    // user can't interact with anything before colorSequence is done
+    // user can't interact with anything before game is on
     app.powerOnGame = false;
 
     if(app.signal === app.level){
@@ -194,47 +205,47 @@ app.init = () => {
     app.fourth();
   }
   app.redButton.addEventListener('click',(e) =>{
-    if(app.powerOnGame){
+    if(app.powerOnGame && app.colorSequence.length > 1){
       app.chosenSequence.push(1);
       app.first();
       if(!app.winner){
         setTimeout(() => {
           app.colorsToDefault();    
-        }, 200);
+        }, 100);
       }
     }
   })
   app.blueButton.addEventListener('click', (e) => {
-    if (app.powerOnGame) {
+    if (app.powerOnGame && app.colorSequence.length > 1) {
       app.chosenSequence.push(2);
       app.second();
       if (!app.winner) {
         setTimeout(() => {
           app.colorsToDefault();
-        }, 200);
+        }, 100);
       }
     }
   })
   app.greenButton.addEventListener('click', (e) => {
-    if (app.powerOnGame) {
+    if (app.powerOnGame && app.colorSequence.length > 1) {
       app.chosenSequence.push(3);
       app.third();
       app.isCorrect();
       if (!app.winner) {
         setTimeout(() => {
           app.colorsToDefault();
-        }, 200);
+        }, 100);
       }
     }
   })
   app.yellowButton.addEventListener('click', (e) => {
-    if (app.powerOnGame) {
+    if (app.powerOnGame && app.colorSequence.length > 1) {
       app.chosenSequence.push(4);
       app.fourth();
       if (!app.winner) {
         setTimeout(() => {
           app.colorsToDefault();
-        }, 200);
+        }, 100);
       }
     }
   })
@@ -258,17 +269,20 @@ app.init = () => {
     // at game end
     if (app.chosenSequence.length === 3 && app.correctPick) {
       app.youWin();
+      document.getElementById("start").onclick = function () {
+        this.disabled = false;
+      }
     }
     if(app.correctPick === false){
       app.signalFlash();
       app.powerOnGame = false;
       app.feedback.innerHTML = '';
       app.levelCount.innerHTML = 
-      `<h3>Wrong choice!</h3> 
-      <h3>You made it to Level ${app.level}.<br> Restart to try again.</h3>
-      <h3>Enter your name below to add your name to the scoreboard</h3>`;
+      `<h3>Wrong choice! You made it to Level ${app.level}. Restart to try again.</h3>
+      <h3>Enter your name below to record your score</h3>`;
       createForm();
       app.clearGame();
+      document.getElementById('start').style.visibility = 'visible';
     }
     
     if(app.level === app.chosenSequence.length && app.correctPick && !app.winner ){
@@ -285,10 +299,12 @@ app.init = () => {
 
   // game is won
   app.youWin = () => {
+    app.feedback.innerHTML = "";
     app.levelCount.innerHTML = `<h3>You won, well done! Enter your name to save your score </h3>`;
     createForm();
     app.powerOnGame = false;
     app.winner = true;
+    document.getElementById('start').style.visibility = 'visible';
   }
 }
 // start the app
